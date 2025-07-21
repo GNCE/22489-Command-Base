da7debffa2f67c60bd2cc281211db74957dc118f
light <lights.darke@gmail.com>
2025-07-21 09:27:51 -0400

    example complex using subsystem base commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBased/commands/commands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBased/commands/commands.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode.CommandBased.commands;
 
+import org.firstinspires.ftc.teamcode.CommandBased.core.Command;
+import org.firstinspires.ftc.teamcode.CommandBased.core.CommandGroups.Parallel;
+import org.firstinspires.ftc.teamcode.CommandBased.core.CommandGroups.Sequential;
+import org.firstinspires.ftc.teamcode.CommandBased.core.Wait;
+import org.firstinspires.ftc.teamcode.CommandBased.subsys.DriveSubsys;
+
 public class commands {
+    public static Command driveForward(DriveSubsys drive) {
+        return drive.driveForwardCommand(0.5, 2.0);
+    }
+    public static Command waitThenDrive(DriveSubsys drive) {
+        return new Sequential(
+                new Wait(0.1),
+                drive.driveForwardCommand(0.5, 2)
+        );
+    }
+    public static Command complexWaitDrive(DriveSubsys drive) {
+        return new Sequential(
+                new Parallel(
+                    new Wait(1), drive.driveForwardCommand(0.2, 0.5)
+                ),
+                new Wait(0.1),
+                drive.driveForwardCommand(0.5, 2)
+        );
+    }
 }
